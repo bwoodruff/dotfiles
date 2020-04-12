ee2aa2c4a7c21ffcde45260f083ba10165dd130c
Ben Woodruff <ben@agilebits.com>
2020-04-12 19:39:58 -0400

    Adding .config dir creation and neofetch config linking

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Check for config dir and make it if it doesn't exist
+
+if ! [ -d $HOME/.config ]; then
+    echo "config dir doesn't exist; making"
+	mkdir $HOME/.config
+fi
+
 # Install oh my zsh
 
 if ! [ -d $HOME/.oh-my-zsh ]; then
@@ -9,15 +16,19 @@ else
     echo "oh my zsh already installed; skipping"
 fi
 
+echo
+
 # Link zshrc
 
 if [ -w $HOME/.zshrc ]; then
-    echo "zshrc already exists; backing up..."
+    echo "zsh config already exists; backing up..."
     mv -f $HOME/.zshrc $HOME/.zshrc.old
 fi
 
 echo "Linking zsh config"
 ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+
+echo
 
 # Install p10k
 
@@ -28,15 +39,19 @@ else
     echo "p10k already installed; skipping"
 fi
 
+echo
+
 # Link p10k config
 
 if [ -w $HOME/.p10k.zsh ]; then
-    echo "p10k already exists; backing up..."
+    echo "p10k config already exists; backing up..."
     mv -f $HOME/.p10k.zsh $HOME/.p10k.zsh.old
 fi
 
 echo "Linking p10k config"
 ln -s $HOME/dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
+
+echo
 
 # Install zsh plugins
 
@@ -57,4 +72,22 @@ if ! [ -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions ]; then
     git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 else
     echo "zsh-completions already installed; skipping"
+fi
+
+echo
+
+# Link neofetch config, if installed
+
+if [[ -x "$(command -v neofetch)" ]]; then
+
+    if [ -w $HOME/.config/neofetch/config.conf ]; then
+        echo "neofetch config already exists; backing up..."
+        mv -f $HOME/.config/neofetch/config.conf $HOME/.config/neofetch/config.conf.old
+    fi
+
+    echo "Linking neofetch config"
+    ln -s $HOME/dotfiles/neofetch/config.conf $HOME/.config/neofetch/config.conf
+    
+    neofetch
+    
 fi
